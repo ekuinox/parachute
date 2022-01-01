6c7c442fb1463dfcceddd2760f8abf35f3c799db
ekuinox <depkey@me.com>
2022-01-01 16:44:58 +0900

    環境変数からパスを取得する

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ mod config;
 
 use config::*;
 use anyhow::Result;
+use dotenv::dotenv;
+use std::env;
+
+const CONFIG_ENV_KEY: &'static str = "PARACHUTE_CONFIG_PATH";
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let conf = Config::try_from_path("./parachute.toml")?;
+    let _ = dotenv();
+    let path = env::var(CONFIG_ENV_KEY)?;
+    let conf = Config::try_from_path(&path)?;
 
     println!("token: {}", conf.discord.token);
 
